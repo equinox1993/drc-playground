ec828f36294c3c5da10608db87b3687d0beec24a
Yuwei Huang <yuwaiw@gmail.com>
2020-09-14 01:19:33 -0700

    Initial commit

---
+++ connect_to_gamepad.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source config/constants.sh
+
+if [[ "$(whoami)" != "root" ]]; then
+  echo "Try again as root"
+  exit 1
+fi
+
+ip addr flush dev "$WLAN"
+# 192.168.1.10: console; 192.168.1.11: gamepad
+ip a a 192.168.1.10/24 dev "$WLAN"
+ip l set mtu 1800 dev "$WLAN"
+
+"$NETBOOT_BIN" 192.168.1.255 192.168.1.10 192.168.1.11 "$GAMEPAD_MAC" &
+NETBOOT_PID="$!"
+
+TMPDIR="$(mktemp -d)"
+
+cat wiiu_ap_normal.conf |
+    sed -e "s/interface=.*/interface=$WLAN/" |
+    sed -e "s/ssid=.*/ssid=$CONSOLE_SSID/" |
+    sed -e "s/wpa_psk=.*/wpa_psk=$CONSOLE_PSK/" > "$TMPDIR/wiiu_ap_normal.conf"
+
+"$HOSTAPT_BIN" -dd "$TMPDIR/wiiu_ap_normal.conf"
+
+kill "$NETBOOT_PID"
+
+rm -r "$TMPDIR"
---
+++ constants_template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO: Copy this file into config/, rename it to constants.sh, and modify the
+# values.
+
+# Binaries
+export HOSTAPT_BIN=~/drc-hostap/hostapd/hostapd
+export NETBOOT_BIN=~/netboot/netboot
+
+export CONSOLE_PSK="00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
+export CONSOLE_SSID="WiiUaabbccddeeff"
+export GAMEPAD_MAC="aa:bb:cc:dd:ee:ff"
+export WLAN="wlan0"
